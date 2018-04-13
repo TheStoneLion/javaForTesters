c051f9bb78c3a3428e7b41a435ba2a42adceb01b
Marc Löwenstein <marc.lowenstein@gmail.com>
2018-04-13 09:47:52 +0200

    Contructor, Getter en Setters

---
+++ src/test/java/Hoofdstuk006/UserTest.java
@@ -0,0 +1,37 @@
+package Hoofdstuk006;
+
+import com.javafortesters.domainentities.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+    @Test
+    public void canConstructANewUser_01() {
+        User user = new User();
+
+        user.setUsername("username");
+        user.setPassword("password");
+
+        assertEquals("default username expected",
+                "username",
+                user.getUsername());
+
+        assertEquals("default password expected",
+        "password",
+                user.getPassword());
+    }
+
+    @Test
+    public void canConstructANewUser_02() {
+        User user = new User("admin", "Welkom01");
+
+        assertEquals("given username expected",
+                "admin",
+                user.getUsername());
+
+        assertEquals("given password expected",
+                "Welkom01",
+                user.getPassword());
+    }
+}
