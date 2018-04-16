8614e54ccb7eac9d947604850cf3ba1f4961e89e
Marc Löwenstein <marc.lowenstein@gmail.com>
2018-04-16 11:07:36 +0200

    Maak array en for each

--- src/test/java/com/javafortesters/Hoofdstuk009/H09O01.java
+++ src/test/java/com/javafortesters/Hoofdstuk009/H09O01.java
@@ -1,7 +1,26 @@
 package com.javafortesters.Hoofdstuk009;
 
 import com.javafortesters.domainentities.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class H09O01 {
-    User[] Gebruikers = new User[3];
+
+    @Test
+    public void ProcessUsers (){
+        User[] gebruikers = new User[3];
+        gebruikers[0] = new User("Marc", "Welkom01");
+        gebruikers[1] = new User("Marieke", "Welkom01");
+        gebruikers[2] = new User("Alexander", "Welkom01");
+        String gebruikersNamen = "";
+
+        for (User gebruiker : gebruikers) {
+            String gebruikersNaam = gebruiker.getUsername();
+            gebruikersNamen = gebruikersNamen + "|" + gebruikersNaam;
+            System.out.println(gebruikersNaam);
+        }
+
+        assertEquals("|Marc|Marieke|Alexander", gebruikersNamen);
+    }
 }
