3917c158f4c3fd1fe40f869e38dca9da5c24b2d5
Marc Löwenstein <marc.lowenstein@gmail.com>
2018-04-16 11:24:44 +0200

    Create an array of 100 users

--- src/test/java/com/javafortesters/Hoofdstuk009/H09O01.java
+++ src/test/java/com/javafortesters/Hoofdstuk009/H09O01.java
@@ -23,4 +23,20 @@ public class H09O01 {
 
         assertEquals("|Marc|Marieke|Alexander", gebruikersNamen);
     }
+
+    @Test
+    public void CreateUsers () {
+        User[] gebruikers = new User[100];
+        for(int i=0; i < 100; i++) {
+            //j = i + 1;
+            gebruikers[i] = new User("user" + (i + 1), "password" + (i + 1));
+        }
+
+        assertEquals("user1", gebruikers[0].getUsername());
+        assertEquals("password1", gebruikers[0].getPassword());
+        assertEquals("user25", gebruikers[24].getUsername());
+        assertEquals("password25", gebruikers[24].getPassword());
+        assertEquals("user100", gebruikers[99].getUsername());
+        assertEquals("password100", gebruikers[99].getPassword());
+    }
 }
